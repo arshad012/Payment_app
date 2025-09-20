2097daa1f8886e5af14c83998ca6c86c1c23c36c
arshad012 <forarshad99@gmail.com>
2025-09-20 22:37:58 +0530

    Added globla URL

--- frontend/src/base_url/index.js
+++ frontend/src/base_url/index.js
@@ -1,3 +1,3 @@
 
-// export const BASE_URL = 'https://payment-app-t5ml.onrender.com';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://payment-app-t5ml.onrender.com';
+// export const BASE_URL = 'http://localhost:3000';
